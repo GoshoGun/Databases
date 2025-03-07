512378cfc3b931ce5436f505e8dc407ebdbc4f30
GoshoGun <gogomaxx@abv.bg>
2025-03-07 21:16:46 +0200

    Update start.java

--- yes/idea/da/start.java
+++ yes/idea/da/start.java
@@ -3,5 +3,7 @@ package yes.idea.da;
 public class start {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello");
+        System.out.println("World");
     }
 }
